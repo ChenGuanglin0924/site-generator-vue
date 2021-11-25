d71e40341978fa901c3c029db3bb7f168e1c63cb
GarasChan <diyad7299@qq.com>
2021-11-25 23:23:05 +0800

    vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,12 +2,25 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: 'site-generator-vue',
-  lang: 'zn-CN',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
 
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      // NavbarGroup
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
   },
 })
